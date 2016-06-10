8ab337f34aa97f2b126fcf575bbf194e98a4784f
mylake <ilake.chang@gmail.com>
2016-06-10 18:00:56 +0800

    Playing with Metal

--- templater/app/controllers/cms_controller.rb
+++ templater/app/controllers/cms_controller.rb
@@ -1,7 +1,14 @@
 class CmsController < ApplicationController
-  prepend_view_path SqlTemplate::Resolver.instance
+# class CmsController < ActionController::Metal
+  include ActionController::Rendering
+  include AbstractController::Helpers
+  prepend_view_path ::SqlTemplate::Resolver.instance
+  # helper CmsHelper
 
   def respond
     render template: params[:page]
   end
 end
+
+# module CmsHelper
+# end
